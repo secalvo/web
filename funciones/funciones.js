1d9bccc0711ed26515acff06d46ce60ba41cc274
secalvo <secalvo@birt.eus>
2025-10-19 20:29:42 +0200

    Update funciones.js

--- funciones/funciones.js
+++ funciones/funciones.js
@@ -12,6 +12,20 @@ function pregunta(){
 		alert("Espero que encuentre pronto un destino");
 	}
 }
+<script>
+function confirmacion() {
+	var pregunta = confirm("¿Deseas visitar la página principal?")
+	if (pregunta){
+		alert("Te envio a la pagina principal")
+		window.location = "https://secalvo.github.io/web/index.html";
+	}
+	else{
+		alert("De acuerdo, nos quedamos aquí")
+	}
+}
+</script>
+<button type="button" onclick="confirmacion()" >Ir a la pagina principal</button>
+
 
 
 
