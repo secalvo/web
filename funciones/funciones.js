cb195d0a431a2fb594d5f621989893cbcb5476eb
secalvo <secalvo@birt.eus>
2025-10-19 18:48:17 +0200

    Update funciones.js

--- funciones/funciones.js
+++ funciones/funciones.js
@@ -6,12 +6,13 @@ function saludo(){
 }
 function pregunta(){
 	var pais = prompt("¿A que pais te gustaria viajar?");
-	if (pais.trim ==="")or (pais ===null){
+	if (pais.trim ===""){
 		alert("Espero que encuentres pronto un destino");
 	}else{
 		alert("Gracias por responder. Excelente elección");
 	}
 }	
+
 
 
 
