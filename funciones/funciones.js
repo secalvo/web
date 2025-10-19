08dcce41469ae24ba2e47897d40b87ae3275f8f4
secalvo <secalvo@birt.eus>
2025-10-19 17:39:10 +0200

    Update funciones.js

--- funciones/funciones.js
+++ funciones/funciones.js
@@ -4,3 +4,22 @@ function mostrarAlerta() {
 function saludo(){
 	alert("¡Bienvenidos a EXPOTRAVEL!");
 }
+function valoracion(){
+	//Ingresamos un mensaje a mostrar
+	var puntuacion = prompt("¿que puntuacion del 0 al 3 le das a la pagina?", "");
+	//Detectamos si el usuario ingreso un valor
+	if (puntuacion = 0){
+		alert("Esperemos mejorar para la proxima vez");
+	}
+	if (puntuacion = 1){
+		alert("Esperemos ser de tua grado la proxima vez");
+	}
+	if (puntuacion = 2){
+		alert("Agradecemos mucho tu opinion");
+	}
+	if puntuacion = 3{
+		alert("¡Excelente! Muchas gracias por tu opinion")
+	}
+else {
+alert("No has ingresado una puntuacion");
+}
